b666c093457880dafe6b5b4fe58015b26af0aa21
nikolemc <nikolemcstanley@gmail.com>
2017-03-22 17:45:32 -0400

    Refactor your number guessing game so that it uses 3 or more method(s), at least 2 of them should have arguments passed in and 2 should return something other than void.

--- RefactorGuessingGame/RefactorGuessingGame/Program.cs
+++ RefactorGuessingGame/RefactorGuessingGame/Program.cs
@@ -8,8 +8,93 @@ namespace RefactorGuessingGame
 {
     class Program
     {
+        static int PromptUser()
+        {
+            Console.WriteLine("Give me your best guess");
+            var input = Console.ReadLine();
+            int.TryParse(input, out int guess);
+            return guess;
+        }
+
+        static bool WasAlreadyGuess(int[] pastGuess, int userGuess) //pastGuess is my array  userGuess is the current guess
+        {
+            bool WasAlreadyGuess = false;
+
+            foreach (var testGuess in pastGuess)  //test guess is my container for testing and comparing
+            {
+                if (userGuess == testGuess)
+                {
+                    WasAlreadyGuess = true;
+                }
+            }
+            return WasAlreadyGuess;
+        }
+
+        static string EndingCredit(int tryvalue)
+        {
+            if (tryvalue > 4)
+            {
+                return "You lost";
+            }
+            else
+            {
+                return "You Won!";
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            var target = new Random().Next(1, 101);
+            Console.WriteLine($"the target is {target}");
+
+            var counter = 0;
+            var guess = 0;
+            var pastGuess = new int[5];
+
+            while (counter < 5 && guess != target)
+            {
+                guess = PromptUser();
+
+                //**wasalreadyguess
+                bool wasAlreadyGuess = false;
+                wasAlreadyGuess = WasAlreadyGuess(pastGuess, guess);
+
+                if (wasAlreadyGuess)
+                {
+                    Console.WriteLine("You already guessed that, try again");
+                }
+                else
+                {
+                    pastGuess[counter] = guess;
+
+                    Console.WriteLine($"your guess was {guess}");
+
+                    if (guess < target)
+                    {
+                        Console.WriteLine("Too low, try again.");
+                    }
+                    else if (guess > target)
+                    {
+                        Console.WriteLine("Too high, try again");
+                    }
+
+                    Console.WriteLine("Your past guesses are:");
+                }
+
+                foreach (var userGuess in pastGuess)
+                {
+                    if (userGuess != 0)
+                    {
+                        Console.Write($"{userGuess},");
+                    }
+                }
+                counter++;
+                
+            }
+
+           Console.WriteLine($"{EndingCredit(counter)}");
+
         }
     }
 }
